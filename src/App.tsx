1336cd60f7415061692dcbf2c84459333e4a574e
Lee Husain <202633387+leehusain-dev@users.noreply.github.com>
2025-11-05 15:42:59 +0000

    Warning data fetched from selected area and region

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,44 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { fetchDatasets } from "./api";
+import {
+  fecthDetails,
+  fetchData,
+  type areaFloodData,
+  type detailedData,
+  type floodWarning,
+} from "./api";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [datasets, setDatasets] = useState<Array<string>>([]);
+  const [floodData, setFloodData] = useState<Array<areaFloodData>>([]);
+  const [regionNames, setRegionNames] = useState<Array<areaFloodData>>([]);
+  const [areaNames, setAreaNames] = useState<Array<string>>([]);
+  const [selectedRegion, setSelectedRegion] = useState<string>("no data");
+  const [selectedArea, setSelectedArea] = useState<string>("no data");
+  const [warningDetail, setWarningDetail] = useState<floodWarning>();
 
   useEffect(() => {
-    fetchDatasets().then(setDatasets);
+    fetchData().then(setFloodData);
   }, []);
+
+  useEffect(() => {
+    const names = floodData.map((region) => region.eaAreaName);
+    setAreaNames([...new Set(names)]);
+  }, [floodData]);
+
+  useEffect(() => {
+    setRegionNames(
+      floodData.filter((item) => item.eaAreaName === selectedArea)
+    );
+  }, [selectedArea]);
+
+  useEffect(() => {
+    if (selectedRegion !== "no data") {
+      fecthDetails(selectedRegion).then((detail) =>
+        setWarningDetail(detail.items.currentWarning)
+      );
+    }
+  }, [selectedRegion]);
 
   return (
     <>
@@ -23,18 +52,20 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <select>
-        {datasets.map((item) => (
+      <select id="areaSelect" onChange={(e) => setSelectedArea(e.target.value)}>
+        {areaNames.map((item) => (
           <option key={item} value={item}>
             {item}
+          </option>
+        ))}
+      </select>
+      <select
+        id="regionSelect"
+        onChange={(e) => setSelectedRegion(e.target.value)}
+      >
+        {regionNames.map((item) => (
+          <option key={item.floodAreaID} value={item.floodAreaID}>
+            {item.description}
           </option>
         ))}
       </select>
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,92 @@
-export const fetchDatasets = async (): Promise<Array<string>> => {
-  const result = await fetch(`https://api.beta.ons.gov.uk/v1/datasets`);
-  const datasets = await result.json();
-  const ids = datasets.items.map((dataset: { id: any }) => dataset.id);
-  return ids;
+export interface areaFloodData {
+  "@id": string;
+  description: string;
+  eaAreaName: string;
+  eaRegionName: string;
+  floodArea: {
+    "@id": string;
+    county: string;
+    notation: string;
+    polygon: string;
+    riverOrSea: string;
+  };
+  floodAreaID: string;
+  isTidal: true;
+  message: string;
+  severity: string;
+  severityLevel: number;
+  timeMessageChanged: Date;
+  timeRaised: Date;
+  timeSeverityChanged: Date;
+}
+
+export const fetchData = async (): Promise<Array<areaFloodData>> => {
+  const result = await fetch(
+    `https://environment.data.gov.uk/flood-monitoring/id/floods`
+  );
+  const dataset = await result.json();
+  return dataset.items;
+};
+
+export interface floodWarning {
+  "@id": string;
+  description: string;
+  eaAreaName: string;
+  eaRegionName: string;
+  floodArea: string;
+  floodAreaID: string;
+  isTidal: boolean;
+  message: string;
+  severity: string;
+  severityLevel: number;
+  timeMessageChanged: Date;
+  timeRaised: Date;
+  timeSeverityChanged: Date;
+  type: string;
+}
+
+export interface detailedData {
+  "@context": string;
+  meta: {
+    publisher: string;
+    licence: string;
+    documentation: string;
+    version: string;
+    comment: string;
+    hasFormat: [string, string, string];
+  };
+  items: {
+    "@id": string;
+    county: string;
+    currentWarning: floodWarning;
+    description: string;
+    eaAreaName: string;
+    envelope: {
+      lowerCorner: {
+        x: number;
+        y: number;
+      };
+      upperCorner: {
+        x: number;
+        y: number;
+      };
+    };
+    fwdCode: string;
+    label: string;
+    lat: number;
+    long: number;
+    notation: string;
+    polygon: string;
+    quickDialNumber: string;
+    riverOrSea: string;
+    type: string[];
+  };
+}
+
+export const fecthDetails = async (id: string): Promise<detailedData> => {
+  const result = await fetch(
+    `https://environment.data.gov.uk/flood-monitoring/id/floodAreas/${id}`
+  );
+  const data = await result.json();
+  return data;
 };
